5254c9209a286c2d9af447d917540150fa910694
Shinhoo Kang <brock.kang@gmail.com>
2026-03-01 15:40:00 +0900

    update

---
+++ 03-Python/example_module.py
@@ -0,0 +1,9 @@
+def greet(name, greeting="Hello"):
+    """이름과 인사말을 출력하는 함수"""
+    return f"{greeting}, {name}!"
+
+print(greet("Alice"))
+print(greet("Bob", "Hi"))
+
+
+scores = [88, 75, 92, 60, 95]
